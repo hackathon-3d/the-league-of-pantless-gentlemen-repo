4c6c6ca9cd471df515e2c0416068a80cd3cd97e5
Spohn <jonathan.spohn@sparcedge.com>
2013-08-23 22:05:14 -0400

    first commit

---
+++ BeerRun/BeerRun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeerRun
+//
+//  Created by Jonathan Spohn on 8/23/13.
+//  Copyright (c) 2013 LeagueOfPantlessGentlemen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeerRun/BeerRun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeerRun
+//
+//  Created by Jonathan Spohn on 8/23/13.
+//  Copyright (c) 2013 LeagueOfPantlessGentlemen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
